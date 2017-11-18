ece00dcd1e8a7b0c9dbc20386c72cfc6cacced5c
Pezhman Nasirifard <epezhman@gmail.com>
2017-11-18 11:50:07 +0100

    all

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var builder = require('botbuilder');
+const restify = require('restify');
+const builder = require('botbuilder');
 
-var connector = new builder.ConsoleConnector().listen();
+const config = require('./config');
+
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: config.MS_APP_ID,
+    appPassword: config.MS_APP_PASS
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
